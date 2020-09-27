119409d3a5329732b23ca4867c179641950af882
Beylull Teklay <beyluleyob@yahoo.com>
2020-09-26 20:02:24 -0400

    first commit

---
+++ Lab01/Assignment2/Javascript2.js
@@ -0,0 +1,28 @@
+/*Assignment2 Javascript
+ * created by Beylull Teklay.
+ * Created on Wednesday,June 17,2020.
+ * Table 
+ * Filename:Javascript2.js
+ */
+
+//function createTable() {
+//    var num_rows = document.getElementById('rows').value;
+//    var num_columns = document.getElementById('columns').value;
+//    var theader = '<table border="1">\n';
+//    var tbody = '';
+
+//    for (var i = 1; i <= num_rows; i++) {
+//        tbody += '<tr>';
+//        for (var j = 1; j <= num_columns; j++) {
+//            tbody += '<td>';
+//            tbody +=  i + ',' + j;
+//            tbody += '</td>'
+//        }
+//        tbody += '</tr>\n';
+//    }
+//    var tfooter = '</table>';
+//    document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
+//}
+
+
+//list.appendChild(location); contact.innerHTML = "location";
