ab29aa5a37c3382bab35a1e3d1692e7e9037b400
Beylull Teklay <beyluleyob@yahoo.com>
2020-11-03 19:27:41 -0500

    updated db URI of mongoDB Atlas

---
+++ COMP229-F2020-MidTerm-301068912/COMP229-F2020-MidTerm-301068912/COMP229-F2020-MidTerm-Start/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+ // "URI": "mongodb://localhost/book_store"
+ "URI": "mongodb+srv://Beylull_:0LqEXFShiIiO8y8m@cluster0.eo3vc.mongodb.net/book_store?retryWrites=true&w=majority"
+};
